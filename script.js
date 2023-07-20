80b520674bb0579d7b5e46545429f16028036c93
Rahul Muddasani <118585444+Rahulmuddasani123@users.noreply.github.com>
2023-07-20 16:17:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('button').addEventListener('click', function() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById('box').style.backgroundColor = color
+})
